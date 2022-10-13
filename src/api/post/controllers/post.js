ace6dc5f5afa456d106795b3262e078f20b370cd
Gabriela Valle <vallefarinha@gmail.com>
2022-10-13 15:56:53 -0300

    count

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,9 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', {
+  count(ctx) {
+      var { query } = ctx.request
+      return strapi.query('api::post.post').count({ where: query });
+  }
+});
